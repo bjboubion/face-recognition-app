da70364b07df8607ab481e450bfbada3d5e13377
bjboubion <bjboubion@gmail.com>
2018-10-24 14:23:44 -0700

    Updates

---
+++ precache-manifest.be8feb5340f5f62e65a0c4ddbb8a56c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6de2bf8da835eb85162e",
+    "url": "/face-recognition-app/static/js/runtime~main.6de2bf8d.js"
+  },
+  {
+    "revision": "a116391f6f93748b205d",
+    "url": "/face-recognition-app/static/js/main.a116391f.chunk.js"
+  },
+  {
+    "revision": "35e5211cd75fa0a0c585",
+    "url": "/face-recognition-app/static/js/1.35e5211c.chunk.js"
+  },
+  {
+    "revision": "a116391f6f93748b205d",
+    "url": "/face-recognition-app/static/css/main.6d06abf2.chunk.css"
+  },
+  {
+    "revision": "35e5211cd75fa0a0c585",
+    "url": "/face-recognition-app/static/css/1.ef97a70d.chunk.css"
+  },
+  {
+    "revision": "c93e8e71f9618ed2115a43a3cdab6edf",
+    "url": "/face-recognition-app/index.html"
+  }
+];
